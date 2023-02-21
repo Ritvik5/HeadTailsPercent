f1b4b69f1282d73b5ccf43c55581179f5717d08e
Ritvik5 <sharma.ritvik56@gmail.com>
2023-02-22 00:07:00 +0530

    Percent of Head and Tail for 10 flips

---
+++ HeadAndTails/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace HeadAndTails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numFlips = 10;
+            int numHead = 0;
+            int numTail = 0;
+            Random random = new Random();
+
+            for(int i = 0;i< numFlips;i++)
+            { 
+                int result=random.Next(0,2);
+                if (result == 0)
+                {
+                    numHead++;
+                }
+                else
+                { 
+                    numTail++;
+                }
+            }
+            double percentHead = (double)numHead / numFlips * 100;
+            double percentTail = (double)numTail / numFlips * 100;
+
+            Console.WriteLine("Heads " + numHead + " (" + percentHead.ToString("0.00") + "%)");
+            Console.WriteLine("Tails " + numTail + " (" + percentTail.ToString("0.00") + "%)");
+        }
+    }
+}
